024066849e04c82e3a25341d61a30b2c190d2c56
srkInturu <103008424+srkInturu@users.noreply.github.com>
2022-04-05 06:02:18 +0530

    Add files via upload

---
+++ spRestAPI/src/main/java/com/sprest/apis/SpRestApiApplication.java
@@ -0,0 +1,16 @@
+package com.sprest.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+
+public class SpRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpRestApiApplication.class, args);
+	}
+
+}
